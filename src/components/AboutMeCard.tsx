9bcc97dea6e651e032fe8c6f5693ae4ed1844bd0
Andrii Naida <work.andrew.naida@gmail.com>
2024-01-26 18:04:17 +0100

    Link fix

--- src/components/AboutMeCard.tsx
+++ src/components/AboutMeCard.tsx
@@ -9,9 +9,11 @@ export const AboutMeCard = ({ className }: AboutMeCardProps) => {
     return (
         <div className={`relative p-1 ${className}`}>
             <div className="absolute inset-0 bg-gradient-to-b from-[#FEFEFE]/30 to-[#8B96B1]/30 rounded-[72px] p-[1px] max-[768px]:rounded-3xl">
-                <div className="relative bg-[#1b2031] w-full h-full rounded-[72px] overflow-hidden font-mono max-[768px]:rounded-3xl">
-                    <Image className="absolute left-16 top-0 w-full h-full" src="/about-me.png" alt="me" width={1500} height={1500} />
-                </div>
+                <Link href="/about-me">
+                    <div className="relative bg-[#1b2031] w-full h-full rounded-[72px] overflow-hidden font-mono max-[768px]:rounded-3xl">
+                        <Image className="absolute left-16 top-0 w-full h-full" src="/about-me.png" alt="me" width={1500} height={1500} />
+                    </div>
+                </Link>
             </div>
         </div>
     )
